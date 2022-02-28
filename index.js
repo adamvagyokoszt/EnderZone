13d36116082bc6ff35db321b1fdb1a8790e6d07e
adamvagyokoszt <88040780+adamvagyokoszt@users.noreply.github.com>
2022-02-28 16:03:25 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+const { Client, Intents, MessageEmbed, Permissions, CommandInteraction, ReactionUserManager, MessageActionRow, MessageButton, Collection } = require("discord.js")
+const client = new Client({ intents: [Intents.FLAGS.GUILDS,Intents.FLAGS.DIRECT_MESSAGE_TYPING,Intents.FLAGS.GUILD_MEMBERS,Intents.FLAGS.GUILD_PRESENCES,Intents.FLAGS.GUILD_MESSAGES] });
+const botconfig = require("./botconfig.json");
+
+let clientname = "EnderZone bot"
+
+client.on("ready", async() => {
+    console.log(`${client.user.username} sikeresn elindult!`)
+
+    let státuszok = [
+        "EnderZone Minecraft szerver"
+    ]
+
+    setInterval(function() {
+        let status = státuszok[Math.floor(Math.random()* státuszok.length)]
+
+        client.user.setActivity(status, {type: "WATCHING"})
+    }, 5000)
+})
+
+
+client.login(process.env.TOKEN)
